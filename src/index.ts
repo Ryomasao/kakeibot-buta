912c12a8534c9fda2651d1b09a85ccc8633df133
tohu.masao <konoemario@gmail.com>
2019-10-26 15:03:23 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import Express from 'express'
+import * as line from '@line/bot-sdk'
+
+const app = Express()
+
+const lineConfig = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN || '',
+  channelSecret: process.env.LINE_CHANNEL_SECRET || '',
+}
+
+app.post('/bot/webhook', line.middleware(lineConfig), (req, res, next) => {
+  res.sendStatus(200)
+  console.log(req.body)
+})
+
+const port = process.env.NODE_ENV === 'development' ? 8888 : 0
+const host = process.env.NODE_ENV === 'development' ? 'localhost' : ''
+
+app.listen(port, host, () => {
+  console.log(process.env.NODE_ENV)
+})
